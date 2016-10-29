e3a2cfc2433f7232bfcc796e78a25da5c6fa7901
Takafumi Arakaki <aka.tkf@gmail.com>
2016-10-28 21:52:45 -0700

    Only warn Pkg.dir() miss-match

--- julia-0.5.0-mkl/run_test.jl
+++ julia-0.5.0-mkl/run_test.jl
@@ -4,4 +4,11 @@ vers = "v$(VERSION.major).$(VERSION.minor)"
 prefix = realpath(ENV["PREFIX"])
 condasite = abspath(prefix, "share", "julia", "site")
 @test JULIA_HOME == abspath(prefix, "bin")
-@test Pkg.dir() == abspath(condasite, vers)
+
+# @test Pkg.dir() == abspath(condasite, vers)
+if Pkg.dir() != abspath(condasite, vers)
+    warn("""
+Pkg.dir() != abspath(condasite, vers) where
+Pkg.dir() = $(Pkg.dir())
+abspath(condasite, vers) = $(abspath(condasite, vers))""")
+end
