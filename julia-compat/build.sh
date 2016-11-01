dc3414a52b39c3fe7b66ca4734e6bcb19e2a6d8f
Takafumi Arakaki <aka.tkf@gmail.com>
2016-10-31 19:30:51 -0700

    Update julia-compat for Julia 0.5

--- julia-compat/build.sh
+++ julia-compat/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ $JULIA_VERSION = "<UNDEFINED>" ]
+if [ "$JULIA_VERSION" = "<UNDEFINED>" -o "$JULIA_VERSION" = "" ]
 then
-    JULIA_VERSION=0.4
+    JULIA_VERSION=0.5
 fi
 JULIA_PKG_NAME=Compat
 DEST="$PREFIX/share/julia/site/v$JULIA_VERSION/$JULIA_PKG_NAME"
